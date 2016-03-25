8bb2c4e299d7f0fb7b4bedba52aaf1a193348571
Aksenov <aksenov.vitaly@gmail.com>
2016-03-25 15:39:31 +0100

    bug with max_index function

---
+++ example/maxindex.cpp
@@ -0,0 +1,46 @@
+/*!
+ * \file max.cpp
+ * \brief Benchmarking script for parallel sorting algorithms
+ * \date 2015
+ * \copyright COPYRIGHT (c) 2015 Umut Acar, Arthur Chargueraud, and
+ * Michael Rainey. All rights reserved.
+ * \license This project is released under the GNU Public License.
+ *
+ */
+
+#include <functional>
+#include "example.hpp"
+#include "io.hpp"
+#include "dpsdatapar.hpp"
+
+/***********************************************************************/
+
+namespace pasl {
+  namespace pctl {
+    void ex() {
+      int n = pasl::util::cmdline::parse_or_default_int("n", 1000);
+      int* x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = i;
+      }
+
+      auto greater = [&] (int x, int y) {
+        return x > y;
+      };
+
+      long index = max_index(x, x + n, 0, greater, [&] (long i, int) {
+        return n - x[i];
+      });
+      std::cerr << index << std::endl;
+    }
+  }
+}
+
+
+ int main(int argc, char** argv) {
+  pbbs::launch(argc, argv, [&] {
+    pasl::pctl::ex();
+  });
+  return 0;
+}
+    
---
+++ example/r.sh
@@ -0,0 +1,46 @@
+split=(${1//./ })
+
+name=${split[0]}
+ext=${split[1]}
+
+cmdline="g++ -std=gnu++11 -g -O0 -I ~/pctl/include -I ~/chunkedseq/include -I ~/pbbs-pctl/include -I ~/quickcheck/quickcheck -I ~/cmdline/include -I ~/pbbs-pctl/example/include"
+
+if [[ $ext == "unkh" ]];
+then
+#  cmdline="${cmdline} -DESTIMATOR_LOGGING -DHONEST -DPCTL_CILK_PLUS -fcilkplus"
+  cmdline="${cmdline} -DESTIMATOR_LOGGING -DHONEST -DTIMING -DPCTL_CILK_PLUS -fcilkplus"
+fi
+
+if [[ $ext == "unko" ]];
+then
+#  cmdline="${cmdline} -DESTIMATOR_LOGGING -DTIMING -DOPTIMISTIC -DPCTL_CILK_PLUS -fcilkplus"
+  cmdline="${cmdline} -DESTIMATOR_LOGGING -DOPTIMISTIC -DTIMING -DPCTL_CILK_PLUS -fcilkplus"
+fi
+
+if [[ $ext == "norm" ]];
+then
+#  cmdline="${cmdline} -DESTIMATOR_LOGGING -DTIMING -DPCTL_CILK_PLUS -fcilkplus"
+  cmdline="${cmdline} -DTIMING -DPCTL_CILK_PLUS -fcilkplus"
+fi
+
+if [[ $ext == "log" ]];
+then
+  cmdline="${cmdline} -DPCTL_CILK_PLUS -fcilkplus -DLOGGING"
+fi
+
+if [[ $ext == "par" ]];
+then
+  cmdline="${cmdline} -DPCTL_CILK_PLUS -fcilkplus -DPCTL_PARALLEL_ELISION"
+fi
+
+if [[ $ext == "seqel" ]];
+then
+  cmdline="${cmdline} -DPCTL_SEQUENTIAL_ELISION"
+fi
+
+if [[ $ext == "seq" ]];
+then
+  cmdline="${cmdline} -DPCTL_SEQUENTIAL_BASELINE"
+fi
+
+eval ${cmdline} ${name}.cpp -o ${name}.${ext}
