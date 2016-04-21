dd8efc8c05c7559ce120f58f0c9fe6880c694894
mikerainey <mike.rainey@inria.fr>
2016-04-21 17:37:43 +0200

    Adding stats and logging support for pasl binaries

--- bench/include/bench.hpp
+++ bench/include/bench.hpp
@@ -56,6 +56,7 @@ namespace pbbs {
 
 #if defined(USE_PASL_RUNTIME)
     pasl::sched::threaddag::init();
+    LOG_BASIC(ENTER_ALGO);
 #endif
 #ifdef PCTL_CILK_PLUS
     int proc = pasl::util::cmdline::parse_or_default_int("proc", 1);
@@ -73,6 +74,10 @@ namespace pbbs {
       body(f);
     });
 #if defined(USE_PASL_RUNTIME)
+    STAT_IDLE(sum());
+    STAT(dump(stdout));
+    STAT_IDLE(print_idle(stdout));
+    LOG_BASIC(EXIT_ALGO);
     pasl::sched::threaddag::destroy();
 #endif
   }
