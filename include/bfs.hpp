ad14e5c01ae599844af184a7cc9ff211e58a6747
Aksenov <aksenov.vitaly@gmail.com>
2016-07-13 20:39:40 +0200

    bfs now works

--- include/bfs.hpp
+++ include/bfs.hpp
@@ -24,6 +24,7 @@
 #include "datapar.hpp"
 #include "graph.hpp"
 #include "timer.hpp"
+#include "sequence.h"
 namespace pasl {
 namespace pctl {
 
@@ -41,7 +42,7 @@ using namespace std;
 
 struct nonNegF{bool operator() (int a) {return (a>=0);}};
 
-pair<int,int> bfs(int start, graph::graph<int>& graph) {
+pair<int,int> bfs(int start, graph::graph<int> graph) {
   timer init_timer;
   init_timer.start();
   int numVertices = graph.n;
@@ -50,7 +51,7 @@ pair<int,int> bfs(int start, graph::graph<int>& graph) {
 #ifdef MANUAL_ALLOCATION
   int* frontier = (int*)malloc(sizeof(int) * numEdges);
   int* visited = (int*)malloc(sizeof(int) * numVertices);
-  parallel_for(0, numVertices, [&] (int i) { visited[i] = 0; });
+  pmem::fill(visited, visited + numVertices, 0);
   int* frontier_next = (int*)malloc(sizeof(int) * numEdges);
 
   int* counts = (int*)malloc(sizeof(int) * numVertices);
@@ -71,6 +72,17 @@ pair<int,int> bfs(int start, graph::graph<int>& graph) {
   int total_visited = 0;
   int round = 0;
   init_timer.end("initialization");
+#ifdef MANUAL_ALLOCATION
+  auto visited_ptr = visited;
+  auto frontier_ptr = frontier;
+  auto frontier_next_ptr = frontier_next;
+  auto counts_ptr = counts;
+#else
+  auto visited_ptr = visited.begin();
+  auto frontier_ptr = frontier.begin();
+  auto frontier_next_ptr = frontier_next.begin();
+  auto counts_ptr = counts.begin();
+#endif
 
   timer scan_timer;
   timer main_timer;
@@ -80,45 +92,89 @@ pair<int,int> bfs(int start, graph::graph<int>& graph) {
     total_visited += frontier_size;
 
     scan_timer.start();
-    parallel_for(0, frontier_size, [&] (int i) {
-	counts[i] = g[frontier[i]].degree;
-    });
+//    parallel_for(0, frontier_size, [&] (int i) {
+//      cilk_for (int i = 0; i < frontier_size; i++)
+//      for (int i = 0; i < frontier_size; i++)
+//	counts[i] = g[frontier[i]].degree;
+//    });
+
+    range::parallel_for(0, frontier_size, [&] (int l, int r) { return r - l; }, [&, counts_ptr, g, frontier_ptr] (int i) {
+        counts_ptr[i] = g[frontier_ptr[i]].degree;
+      }, [&, counts_ptr, g, frontier_ptr] (int l, int r) {
+        for (int i = l; i < r; i++) {
+          counts_ptr[i] = g[frontier[i]].degree;
+        }
+      }
+    );
 #ifdef MANUAL_ALLOCATION
+#ifdef PBBS_SEQUENCE
+    int nr = pbbs::sequence::scan(counts, counts, frontier_size, [&] (int x, int y) { return x + y; }, 0);
+#else
     int nr = dps::scan(counts, counts + frontier_size, 0, [&] (int x, int y) { return x + y; }, counts, forward_exclusive_scan);
+#endif
+#else
+#ifdef PBBS_SEQUENCE
+    int nr = pbbs::sequence::scan(counts.begin(), counts.begin(), frontier_size, [&] (int x, int y) { return x + y; }, 0);
 #else
     int nr = dps::scan(counts.begin(), counts.begin() + frontier_size, 0, [&] (int x, int y) { return x + y; }, counts.begin(), forward_exclusive_scan);
+#endif
 #endif
     scan_timer.end();
 
     main_timer.start();
     // For each vertexB in the frontier try to "hook" unvisited neighbors.
-    range::parallel_for(0, frontier_size, [&] (int l, int r) { return (r == frontier_size ? nr : counts[r]) - counts[l]; },[&] (int i) {
+//    range::parallel_for(0, frontier_size, [&] (int l, int r) { return (r == frontier_size ? nr : counts[r]) - counts[l]; }, [&] (int i) {
 //    cilk_for (int i = 0; i < frontier_size; i++) {
-      int k = 0;
-      int v = frontier[i];
-      int o = counts[i];
+//    for (int i = 0; i < frontier_size; i++) {
+     range::parallel_for(0, frontier_size, [&] (int l, int r) { return (r == frontier_size ? nr : counts_ptr[r]) - counts_ptr[l]; }, [&, frontier_next_ptr, frontier_ptr, g, visited_ptr] (int i) {
+       int k = 0;
+       int v = frontier_ptr[i];
+       int o = counts_ptr[i];
 
-      for (int j = 0; j < g[v].degree; j++) {
-        int ngh = g[v].Neighbors[j];
-	if (visited[ngh] == 0 && utils::CAS(&visited[ngh], 0, 1)) {
-	  frontier_next[o + j] = g[v].Neighbors[k++] = ngh;
-	}
-	else frontier_next[o + j] = -1;
+       for (int j = 0; j < g[v].degree; j++) {
+         int ngh = g[v].Neighbors[j];
+           if (visited_ptr[ngh] == 0 && utils::CAS(&visited_ptr[ngh], 0, 1)) {
+             frontier_next_ptr[o + j] = g[v].Neighbors[k++] = ngh;
+           }
+         else frontier_next_ptr[o + j] = -1;}
+        g[v].degree = k;
+     }, [&, frontier_next_ptr, frontier_ptr, g, visited_ptr] (int l, int r) {
+       for (int i = l; i < r; i++) {
+         int k = 0;
+         int v = frontier_ptr[i];
+         int o = counts_ptr[i];
+
+        for (int j = 0; j < g[v].degree; j++) {
+          int ngh = g[v].Neighbors[j];
+          if (visited_ptr[ngh] == 0 && utils::CAS(&visited_ptr[ngh], 0, 1)) {
+            frontier_next_ptr[o + j] = g[v].Neighbors[k++] = ngh;
+          }
+          else frontier_next_ptr[o + j] = -1;
+        }
+        g[v].degree = k;
       }
-      g[v].degree = k;
-//    }
     });
+//      }}
+
     main_timer.end();
     filter_timer.start();
     // Filter out the empty slots (marked with -1)
 #ifdef MANUAL_ALLOCATION
+#ifdef PBBS_SEQUENCE
+    frontier_size = pbbs::sequence::filter(frontier_next, frontier, nr, [&] (int v) { return v >= 0; });
+#else
     frontier_size = dps::filter(frontier_next, frontier_next + nr, frontier, [&] (int v) { return v >= 0; });
+#endif
+#else
+#ifdef PBBS_SEQUENCE
+    frontier_size = pbbs::sequence::filter(frontier_next.begin(), frontier.begin(), nr, [&] (int v) { return v >= 0; });
 #else
     frontier_size = dps::filter(frontier_next.begin(), frontier_next.begin() + nr, frontier.begin(), [&] (int v) { return v >= 0; });
 #endif
+#endif
     filter_timer.end();
   }
-  std::cerr << total_visited << " " << round << std::endl;
+//  std::cerr << total_visited << " " << round << std::endl;
   scan_timer.report_total("scan total");
   main_timer.report_total("main total");
   filter_timer.report_total("filter total");
