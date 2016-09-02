d07f47721e245db6c8dd019fecdbb7cba2f6e964
Mike Rainey <mike.rainey@inria.fr>
2016-09-02 18:54:51 +0200

    Introducing nested parallelism benchmark; not yet tested

--- bench/granularity.cpp
+++ bench/granularity.cpp
@@ -26,12 +26,8 @@ public:
   char b[szb];
 };
 
-template <int szb>
-inline
-bool operator==(const bytes<szb>& lhs, const bytes<szb>& rhs) {
-  assert(false);
-  return false;
-}
+template <class Item>
+using segment_descriptor = std::pair<Item*, int>; // pair of item start pointer, offset
 
 namespace pasl {
 namespace pctl {
@@ -184,11 +180,73 @@ namespace parallel_with_oracle_guided_and_seq_alt_body {
 namespace parallel_with_level1_reduce {
   
   template <class Item, class Predicate>
-  int nb_occurrences_rec(Item* lo, Item* hi, const Predicate& p) {
+  int nb_occurrences(Item* lo, Item* hi, const Predicate& p) {
     return level1::reduce(lo, hi, 0, [&] (int x, int y) {
       return x + y;
     }, [&] (Item& i) {
       return (p(i)) ? 1 : 0;
+    });
+  }
+  
+} // end namespace
+  
+template <class Pointer, class Weight, class Predicate>
+int nb_occurrences_seq(int lo, int hi, Pointer d, const Weight& w, const Predicate& p) {
+  int r = 0;
+  for (int i = lo; i < hi; i++) {
+    auto v = d[i];
+    r += nb_occurrences_seq(v.first, v.second, p);
+  }
+  return r;
+}
+  
+namespace nested_parallel_with_gc {
+  
+  int threshold;
+
+  template <class Pointer, class Weight, class Predicate>
+  int nb_occurrences(int lo, int hi, Pointer d, const Weight& w, const Predicate& p) {
+    int r = 0;
+    if (w(lo, hi, d) <= threshold) {
+      for (int i = lo; i < hi; i++) {
+        auto v = d[i];
+        r += with_gc::nb_occurrences_rec(v.first, v.second, p);
+      }
+    } else {
+      int mid = (lo + hi) / 2;
+      int r1, r2;
+      primitive_fork2([&] {
+        r1 = nb_occurrences(lo, mid, d, w, p);
+      }, [&] {
+        r2 = nb_occurrences(mid, hi, d, w, p);
+      });
+      r = r1 + r2;
+    }
+    return r;
+  }
+  
+} // end namespace
+  
+namespace nested_parallel_with_level2 {
+  
+  int threshold;
+  
+  template <class Pointer, class Weight, class Predicate>
+  int nb_occurrences(int lo, int hi, Pointer d, const Weight& w, const Predicate& p) {
+    using segment_descriptor_type = segment_descriptor<typename Pointer::value_type>;
+    segment_descriptor_type* sd = d.segdes;
+    auto lo2 = sd + lo;
+    return level2::reduce(lo2, sd + hi, 0, [&] (int x, int y) {
+      return x + y;
+    }, [&] (segment_descriptor_type* lo3, segment_descriptor_type* hi3) {
+      return w(lo3 - lo2, hi3 - lo2, d);
+    }, [&] (long i, segment_descriptor_type&) {
+      auto v = d[i];
+      return with_oracle_guided::nb_occurrences_rec(v.first, v.second, p);
+    }, [&] (segment_descriptor_type* lo3, segment_descriptor_type* hi3) {
+      auto lo4 = lo3 - lo2;
+      auto hi4 = hi3 - lo2;
+      return nb_occurrences_seq(lo4, hi4, d, w, p);
     });
   }
   
@@ -223,6 +281,23 @@ Item* create_random_array(int n) {
   auto hi = data + n;
   write_random_data(lo, hi);
   return data;
+}
+
+template <class Item>
+std::pair<segment_descriptor<Item>*, Item*> create_nested_array(int n) {
+  std::pair<segment_descriptor<Item>*, Item*> r;
+  auto rows = (segment_descriptor<Item>*)malloc(sizeof(segment_descriptor<Item>) * (n + 1));
+  auto bits = create_random_array<Item>(n * (n + 1) / 2);
+  int o = 0;
+  Item* p = bits;
+  for (int i = 0; i < n + 1; i++) {
+    new (&rows[i]) segment_descriptor<Item>(p, o);
+    p += i;
+    o += i;
+  }
+  r.first = rows;
+  r.second = bits;
+  return r;
 }
 
 template <class Item>
@@ -286,7 +361,7 @@ void benchmark(Item* lo, Item* hi, const Predicate& p, pbbs::measured_type measu
   });
   d.add("parallel_with_level1_reduce", [&] {
     measure([&] {
-      result = parallel_with_level1_reduce::nb_occurrences_rec(lo, hi, p);
+      result = parallel_with_level1_reduce::nb_occurrences(lo, hi, p);
     });
   });
   d.dispatch("algorithm");
@@ -296,38 +371,126 @@ void benchmark(Item* lo, Item* hi, const Predicate& p, pbbs::measured_type measu
   assert(result == nb_occurrences_seq(lo, hi, p));
 }
   
+template <class Pointer, class Weight, class Predicate>
+void benchmark(int lo, int hi, Pointer d, const Weight& w, const Predicate& p,
+               pbbs::measured_type measure) {
+  int result;
+  cmdline::dispatcher dp;
+  dp.add("nested_parallel_gc", [&] {
+    measure([&] {
+      result = nested_parallel_with_gc::nb_occurrences(lo, hi, d, w, p);
+    });
+  });
+  dp.add("nested_parallel_with_level2", [&] {
+    measure([&] {
+      result = nested_parallel_with_level2::nb_occurrences(lo, hi, d, w, p);
+    });
+  });
+  dp.dispatch("algorithm");
+  assert(result == nb_occurrences_seq(lo, hi, d, w, p));
+}
+
+bool is_nested() {
+  std::string s = cmdline::parse_or_default<std::string>("algorithm", "");
+  return s.find("nested") != std::string::npos;
+}
+  
+template <class Item>
+class indexer {
+public:
+  
+  using value_type = Item;
+  
+  segment_descriptor<Item>* segdes;
+  
+  indexer(segment_descriptor<Item>* segdes) : segdes(segdes) { }
+  
+  std::pair<Item*, Item*> operator[] (const int i) {
+    auto p = segdes[i];
+    auto lo = p.first;
+    auto sz = segdes[i + 1].second - p.second;
+    auto hi = lo + sz;
+    return std::make_pair(lo, hi);
+  }
+
+};
+
+template <int szb>
+inline
+bool operator==(const bytes<szb>& lhs, const bytes<szb>& rhs) {
+  assert(false);
+  return false;
+}
+  
 template <class Item>
 void benchmark(pbbs::measured_type measure) {
   int n = cmdline::parse<int>("n");
   int nb_hash_iters = cmdline::parse_or_default("nb_hash_iters", 0);
-  Item* data = create_random_array<Item>(n);
-  auto lo = data;
-  auto hi = lo + n;
-  auto c = *lo;
-  if (! cmdline::parse<bool>("use_hash")) {
-    assert(sizeof(Item) == sizeof(char));
-    auto p = [c] (Item d) {
-      return d == c;
-    };
-    benchmark(lo, hi, p, measure);
-  } else if (nb_hash_iters == 0) {
-    auto h = hash(c);
-    auto p = [h] (Item& d) {
-      return h == hash(d);
-    };
-    benchmark(lo, hi, p, measure);
+  if (! is_nested()) {
+    Item* data = create_random_array<Item>(n);
+    auto lo = data;
+    auto hi = lo + n;
+    auto c = *lo;
+    if (! cmdline::parse<bool>("use_hash")) {
+      assert(sizeof(Item) == sizeof(char));
+      auto p = [c] (Item& d) {
+        return d == c;
+      };
+      benchmark(lo, hi, p, measure);
+    } else if (nb_hash_iters == 0) {
+      auto h = hash(c);
+      auto p = [h] (Item& d) {
+        return h == hash(d);
+      };
+      benchmark(lo, hi, p, measure);
+    } else {
+      auto h = hash(c);
+      auto p = [h,nb_hash_iters] (Item& d) {
+        unsigned int hh = hash(d);
+        for (int i = 0; i < nb_hash_iters; i++) {
+          hh = hash(hh);
+        }
+        return h == hh;
+      };
+      benchmark(lo, hi, p, measure);
+    } 
+    free(data);
   } else {
-    auto h = hash(c);
-    auto p = [h,nb_hash_iters] (Item& d) {
-      unsigned int hh = hash(d);
-      for (int i = 0; i < nb_hash_iters; i++) {
-        hh = hash(hh);
-      }
-      return h == hh;
+    int lo = 0;
+    int hi = n;
+    std::pair<segment_descriptor<Item>*, Item*> data = create_nested_array<Item>(n);
+    indexer<Item> idxr(data.first);
+    auto c = *data.second;
+    auto w = [&] (int lo, int hi, indexer<Item> idxr){
+      auto d = idxr.segdes;
+      return d[hi].second - d[lo].second;
     };
-    benchmark(lo, hi, p, measure);
-  } 
-  free(data);
+    if (! cmdline::parse<bool>("use_hash")) {
+      assert(sizeof(Item) == sizeof(char));
+      auto p = [c] (Item& d) {
+        return d == c;
+      };
+      benchmark(lo, hi, idxr, w, p, measure);
+    } else if (nb_hash_iters == 0) {
+      auto h = hash(c);
+      auto p = [h] (Item& d) {
+        return h == hash(d);
+      };
+      benchmark(lo, hi, idxr, w, p, measure);
+    } else {
+      auto h = hash(c);
+      auto p = [h,nb_hash_iters] (Item& d) {
+        unsigned int hh = hash(d);
+        for (int i = 0; i < nb_hash_iters; i++) {
+          hh = hash(hh);
+        }
+        return h == hh;
+      };
+      benchmark(lo, hi, idxr, w, p, measure);
+    }
+    free(data.first);
+    free(data.second);
+  }
 }
   
 template <class Item>
