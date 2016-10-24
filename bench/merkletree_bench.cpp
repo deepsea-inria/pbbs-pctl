8f7b8be103994f2e3a00901e4ef2dc07bd8242ac
Mike Rainey <mike.rainey@inria.fr>
2016-10-24 20:07:39 +0200

    Adding parallel merkle tree construction

--- bench/merkletree_bench.cpp
+++ bench/merkletree_bench.cpp
@@ -55,10 +55,10 @@ Int parent(Int i) {
 }
 
 template <class Block_iterator, class Hash, class Hash_fn, class Hash_block_fn>
-void merkletree(Block_iterator lo, Block_iterator hi, Hash* merkle_lo,
-                const Hash_fn& hash_fn,
-                const Hash_block_fn& hash_block_fn) {
-  auto row_lo = round_up_to_power_of_2((unsigned)(hi - lo));
+void merkletree_seq(Block_iterator lo, Block_iterator hi, Hash* merkle_lo,
+                    const Hash_fn& hash_fn,
+                    const Hash_block_fn& hash_block_fn) {
+  unsigned row_lo = round_up_to_power_of_2((unsigned)(hi - lo));
   int i = 0;
   for (auto it = lo; it != hi; it++, i++) {
     hash_block_fn(it, merkle_lo + row_lo + i);
@@ -73,13 +73,33 @@ void merkletree(Block_iterator lo, Block_iterator hi, Hash* merkle_lo,
   }
 }
   
+template <class Block_iterator, class Hash, class Hash_fn, class Hash_block_fn>
+void merkletree_par(Block_iterator lo, Block_iterator hi, Hash* merkle_lo,
+                    const Hash_fn& hash_fn,
+                    const Hash_block_fn& hash_block_fn) {
+  unsigned row_lo = round_up_to_power_of_2((unsigned)(hi - lo));
+  pasl::pctl::parallel_for(lo, hi, [&] (Block_iterator it) {
+    auto i = it - lo;
+    hash_block_fn(it, merkle_lo + row_lo + i);
+  });
+  for (row_lo = parent(row_lo); row_lo != 0; row_lo = parent(row_lo)) {
+    pasl::pctl::parallel_for(row_lo, 2 * row_lo, [&] (unsigned i) {
+      size_t l = left_child(i);
+      size_t r = right_child(i);
+      hash_fn(merkle_lo + l, merkle_lo + r, merkle_lo + i);
+    });
+  }
+}
+  
 template <class Block_iterator, class Hash_policy>
 typename Hash_policy::digest_type* merkletree(pbbs::measured_type measure,
                                               Block_iterator lo, Block_iterator hi,
-                                              const Hash_policy& hash_policy) {
+                                              const Hash_policy& hash_policy,
+                                              unsigned& nb) {
   using hash_type = typename Hash_policy::digest_type;
-  auto n = round_up_to_power_of_2((unsigned)(hi - lo));
-  hash_type* merkle = malloc_array<hash_type>(2 * n);
+  unsigned n = round_up_to_power_of_2((unsigned)(hi - lo));
+  nb = 2 * n;
+  hash_type* merkle = malloc_array<hash_type>(nb);
   auto hash_fn = [&] (hash_type* lhs, hash_type* rhs, hash_type* dst) {
     Hash_policy::hash_2(lhs, rhs, dst);
   };
@@ -91,10 +111,31 @@ typename Hash_policy::digest_type* merkletree(pbbs::measured_type measure,
   cmdline::dispatcher d;
   d.add("sequential", [&] {
     measure([&] {
-      merkletree(lo, hi, merkle, hash_fn, hash_block_fn);
+      merkletree_seq(lo, hi, merkle, hash_fn, hash_block_fn);
+    });
+  });
+  d.add("parallel", [&] {
+    measure([&] {
+      merkletree_par(lo, hi, merkle, hash_fn, hash_block_fn);
     });
   });
   d.dispatch("algorithm");
+  if (cmdline::parse_or_default_bool("check", false)) {
+    hash_type* merkle_ref = malloc_array<hash_type>(nb);
+    merkletree_seq(lo, hi, merkle_ref, hash_fn, hash_block_fn);
+    auto it2 = merkle;
+    int nb_bogus = 0;
+    int i = 0;
+    for (auto it1 = merkle_ref; it1 != (merkle_ref + nb); it1++, it2++, i++) {
+      if (! hash_type::same(*it1, *it2)) {
+        nb_bogus++;
+      }
+    }
+    if (nb_bogus > 0) {
+      std::cout << "There were bogus results: nb_bogus = " << nb_bogus << std::endl;
+    }
+    free(merkle_ref);
+  }
   return merkle;
 }
   
@@ -106,6 +147,20 @@ public:
     assert(i >= 0);
     assert(i < szb);
     return contents[i];
+  }
+  template <class T>
+  T& cast_to() {
+    return *((T*)&contents[0]);
+  }
+  static
+  bool same(byte_array& lhs, byte_array& rhs) {
+    bool s = true;
+    for (int i = 0; i < szb; i++) {
+      if (lhs.contents[i] != rhs.contents[i]) {
+        s = false;
+      }
+    }
+    return s;
   }
 };
   
@@ -138,26 +193,55 @@ template <class Hash_policy>
 void merkletree(pbbs::measured_type measure, const Hash_policy& hash_policy) {
   using hash_type = typename Hash_policy::digest_type;
   hash_type* merkle = nullptr;
+  unsigned nb = 0;
   int block_szb = cmdline::parse<int>("block_szb");
   int nb_blocks = 1 << cmdline::parse<int>("nb_blocks_lg");
   if (block_szb == one_kb) {
     std::vector<small_block> blocks(nb_blocks);
     initialize_blocks(blocks.begin(), blocks.end());
-    merkle = merkletree(measure, blocks.begin(), blocks.end(), hash_policy);
+    merkle = merkletree(measure, blocks.begin(), blocks.end(), hash_policy, nb);
   } else if (block_szb == ten_kb) {
     std::vector<medium_block> blocks(nb_blocks);
     initialize_blocks(blocks.begin(), blocks.end());
-    merkle = merkletree(measure, blocks.begin(), blocks.end(), hash_policy);
+    merkle = merkletree(measure, blocks.begin(), blocks.end(), hash_policy, nb);
   } else if (block_szb == one_mb) {
     std::vector<large_block> blocks(nb_blocks);
     initialize_blocks(blocks.begin(), blocks.end());
-    merkle = merkletree(measure, blocks.begin(), blocks.end(), hash_policy);
+    merkle = merkletree(measure, blocks.begin(), blocks.end(), hash_policy, nb);
   } else {
     std::cerr << "Bogus block_szb " << block_szb << std::endl;
     exit(0);
   }
+  if (cmdline::parse_or_default_bool("print", false)) {
+    int i = 0;
+    for (auto it = merkle; it != merkle + nb; it++, i++) {
+      std::cout << "[" << i << ", " << it->template cast_to<unsigned int>() << "] ";
+    }
+    std::cout << std::endl;
+  }
   free(merkle);
 }
+  
+class pbbs32 {
+public:
+  
+  using digest_type = byte_array<sizeof(unsigned int)>;
+  
+  static
+  void hash_2(digest_type* lhs, digest_type* rhs, digest_type* dst) {
+    dst->cast_to<unsigned int>() = prandgen::hashi(lhs->cast_to<unsigned int>() | rhs->cast_to<unsigned int>());
+  }
+  
+  static
+  void hash_range(const unsigned char* lo, const unsigned char* hi, digest_type* dst) {
+    unsigned int h = 0;
+    for (unsigned int* it = (unsigned int*)lo; it < (unsigned int*)hi; it++) {
+      h = prandgen::hashi(h | *it);
+    }
+    dst->cast_to<unsigned int>() = h;
+  }
+  
+};
 
 class sha256 {
 public:
@@ -183,6 +267,9 @@ public:
   
 void merkletree(pbbs::measured_type measure) {
   cmdline::dispatcher d;
+  d.add("pbbs32", [&] {
+    merkletree(measure, pbbs32());
+  });
   d.add("sha256", [&] {
     merkletree(measure, sha256());
   });
