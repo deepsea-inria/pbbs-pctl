4f973d99f55ac70c2ffd1041ce8c998505438015
Aksenov <aksenov.vitaly@gmail.com>
2017-03-27 18:09:57 +0200

    working properly with structs

--- bench/include/teststructs.hpp
+++ bench/include/teststructs.hpp
@@ -12,6 +12,23 @@ public:
   pasl::pctl::parray<point3d> points;
   pasl::pctl::parray<triangle> triangles;
   pasl::pctl::parray<ray<point3d>> rays;
+
+  ray_cast_test(): points(0), triangles(0), rays(0) { }
+
+  ray_cast_test(const ray_cast_test& other) {
+    points = other.points;
+    triangles = other.triangles;
+    rays = other.rays;
+  }
+
+  ray_cast_test& operator=(const ray_cast_test& other) {
+    if (&other == this) {
+      return *this;
+    }
+    this->points = other.points;
+    this->triangles = other.triangles;
+    this->rays = other.rays;
+  }
 };
 
 }
