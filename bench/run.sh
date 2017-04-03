c09055f17f5909041507575b9f027755790a594a
Aksenov <aksenov.vitaly@gmail.com>
2017-04-03 18:51:15 +0200

    run bash script

---
+++ bench/run.sh
@@ -0,0 +1,11 @@
+./bench.pbench compare -benchmark comparison_sort,blockradix_sort,remove_duplicates,suffix_array,convex_hull,nearest_neighbours,ray_cast -size large -runs 30 -proc 40 -exts unks100
+./bench.pbench compare -benchmark delaunay -size large -runs 30 -proc 40 -exts unks100
+#./bench.pbench compare -benchmark delaunay_refine -size large -runs 6 -proc 40 -exts unks100
+./bench.pbench bfs -proc 40 -runs 30
+
+#./bench.pbench compare -benchmark comparison_sort,blockradix_sort,remove_duplicates,suffix_array,convex_hull,nearest_neighbours,ray_cast -size large -runs 6 -proc 39,40 -exts unks50,unks100,unks200
+#./bench.pbench compare -benchmark delaunay -size large -runs 6 -proc 39,40 -exts unks50,unks100,unks200
+#./bench.pbench compare -benchmark delaunay_refine -size large -runs 6 -proc 39,40 -exts unks50,unks100,unks200
+#./bench.pbench compare -benchmark comparison_sort,blockradix_sort,remove_duplicates,suffix_array,convex_hull,nearest_neighbours,ray_cast,bfs -size large -runs 3 -proc 1,10,20,30,39,40 -exts unks100,unks
+#./bench.pbench compare -benchmark delaunay -size large -runs 3 -proc 1,10,20,30,39,40 -exts unks100,unks
+#./bench.pbench compare -benchmark delaunay_refine -size large -runs 3 -proc 1,10,20,30,39,40 -exts unks100,unks
