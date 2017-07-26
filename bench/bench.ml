7e9bf30cb170f4ed78ae555fe8bc5ede3a6c4adc
Mike Rainey <bionic.oldsmobile@gmail.com>
2017-07-26 22:49:16 +0200

    disabling benchmark remove-duplicates

--- bench/bench.ml
+++ bench/bench.ml
@@ -177,7 +177,7 @@ let arg_sizes =
    | ["all"] -> ["small"; "medium"; "large"]
    | _ -> arg_sizes
 
-let sequence_benchmarks = ["blockradix_sort"; "comparison_sort"; "remove_duplicates";
+let sequence_benchmarks = ["blockradix_sort"; "comparison_sort"; (*"remove_duplicates";*)
                       "suffix_array"; "convex_hull"; "nearest_neighbours"; "ray_cast"; "delaunay"; (*"delaunay_refine"; "bfs"; *) ]
 
 let arg_benchmarks = 
