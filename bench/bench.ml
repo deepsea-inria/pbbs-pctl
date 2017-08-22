83bccc00015df9aba1ca83c4a1074b9036ca69d5
Mike Rainey <mike.rainey@inria.fr>
2017-08-22 23:10:51 +0200

    adding mis to list

--- bench/bench.ml
+++ bench/bench.ml
@@ -183,7 +183,7 @@ let arg_sizes =
    | _ -> arg_sizes
 
 let sequence_benchmarks = ["blockradix_sort"; "comparison_sort"; "remove_duplicates";
-                      "suffix_array"; "convex_hull"; "nearest_neighbours"; "ray_cast"; "delaunay"; (*"delaunay_refine"; "bfs"; *) ]
+                      "suffix_array"; "convex_hull"; "nearest_neighbours"; "ray_cast"; "delaunay"; "mis"; (*"delaunay_refine"; "bfs"; *) ]
 
 let arg_benchmarks = 
    match arg_benchmarks with
